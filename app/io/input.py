0ef180925b9c70a223bb2af2513aa71ad5a04553
Medovshchuk Lera <leramed963@gmail.com>
2024-03-26 12:35:48 +0200

    add output/input function

---
+++ app/io/input.py
@@ -0,0 +1,8 @@
+def console_input(text=''):
+    pass
+
+def file_input(file_name):
+    pass
+
+def pandas_input(file_name):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,5 @@
+def console_output(text):
+    pass
+
+def file_output(file_name, text):
+    pass
